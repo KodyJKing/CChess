9b3dcf5e9d34af23a64074f05e2c09784fa6fba2
KodyJKing <kody.j.king@gmail.com>
2017-04-15 02:03:22 -0700

    Broke stuff.

--- main.c
+++ main.c
@@ -16,19 +16,21 @@ const int BLACK = 1;
 
 typedef struct {
     unsigned int type:3, color:1, moved:1;
-} piece;
+} Piece;
 
-int indexOf(int x, int y) {
-    return x + y * 8;
-}
+typedef struct {
+    unsigned int x:4, y:4;
+} coord;
 
-int xOf(int index) {
-    return index % 8;
+int indexOf(coord pos) { return pos.x + pos.y * 8; }
+coord posOf(int index) {
+    coord pos;
+    pos.x = index % 8;
+    pos.y = ( index - pos.x ) / 8;
+    return pos;
 }
-
-int yOf(int index) {
-    return (index - (index % 8)) / 8;
-}
+char xChar(int x) {return 'a' + x; }
+char yChar(int y) {return '0' + y; }
 
 // ---- Printing ---- //
 
@@ -46,17 +48,17 @@ int charToType(char c) {
 }
 
 int charToPiece(char c) {
-    piece p;
+    Piece p;
     p.type = charToType(c);
     p.color = (c == tolower(c)) ? WHITE : BLACK;
 }
 
-char pieceToChar(piece p) {
+char pieceToChar(Piece p) {
     char result = "kqbnrp."[p.type];
     return p.color == WHITE ? result : toupper(result);
 }
 
-void printBoard(piece board[64]) {
+void printBoard(Piece board[64]) {
     int i;
     printf("\n   abcdefgh\n");
     for( i = 0; i < 64; i++ ) {
@@ -69,34 +71,43 @@ void printBoard(piece board[64]) {
 // ---- Logic ---- //
 
 char backrow[] = "rnbqkbnr";
-
-void initBoard(piece board[64]) {
+void initBoard(Piece board[64]) {
     int i;
-    piece p;
+    coord pos;
+    Piece p;
 
     p.type = EMPTY;
     p.color = BLACK;
     for ( i = 0; i < 64; i++) board[i] = p;
 
-    for ( i = 0; i < 8; i++) {
-        p.type = charToType(backrow[i]);
-        board[indexOf(i, 0)] = p;
-        p.type = PAWN;
-        board[indexOf(i, 1)] = p;
-    }
+    for ( pos.x = 0; pos.x < 8; pos.x++) {
+        p.color = BLACK;
 
-    p.color = WHITE;
-    for ( i = 0; i < 8; i++) {
-        p.type = charToType(backrow[i]);
-        board[indexOf(i, 7)] = p;
+        pos.y = 0;
+        p.type = charToType(backrow[pos.x]);
+        board[indexOf(pos)] = p;
+
+        pos.y = 1;
         p.type = PAWN;
-        board[indexOf(i, 6)] = p;
+        board[indexOf(pos)] = p;
+
+        p.color = WHITE;
+
+        pos.y = 7;
+        p.type = charToType(backrow[pos.x]);
+        board[indexOf(pos)] = p;
+
+        pos.y = 6;
+        p.type = PAWN;
+        board[indexOf(pos)] = p;
     }
 }
 
+// ---- Implementation ---- //
+
 int main()
 {
-    piece board[64];
+    Piece board[64];
     initBoard(&board);
     printBoard(&board);
     return 0;
